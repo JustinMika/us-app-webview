158efb40cb8b0a2492fde38951659b41712f3116
Justin Micah <jmika734@gmail.com>
2023-05-30 09:19:26 +0200

    update

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import {StatusBar} from 'expo-status-bar'
 import {SafeAreaView, StyleSheet} from 'react-native'
 import {WebView} from 'react-native-webview'
+import { Platform } from 'react-native';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+// import PushNotification from 'react-native-push-notification';
+// import notifee, { EventType } from '@notifee/react-native';
 
 export default function App() { 
+    if (Platform.OS === 'ios') {
+        PushNotificationIOS.setApplicationIconBadgeNumber(3);
+      } else if (Platform.OS === 'android') {
+      }
+    //   notifee.getBadgeCount().then(count => console.log('Current badge count: ', count));
+    //   notifee.setBadgeCount(10).then(() => console.log('Badge count removed'));
     const url = "https://ulizaservices.com/"
     // gp5u-yCMULDuC@4b
     return (
         <SafeAreaView style={
             {
                 flex: 1,
-                paddingTop: 40
+                // paddingTop: 40
             }
         }>
             <StatusBar style='auto'/>
